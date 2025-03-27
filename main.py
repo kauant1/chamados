d29743f2d2336e175051b877850bc4b8135c8b42
Dev_kauan <infra_t1@outlook.com.br>
2025-03-27 11:46:51 -0300

    .

--- main.py
+++ main.py
@@ -50,6 +50,7 @@ class TaskEdit(BaseModel):
     field: str
     new_value: str
 
+
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
